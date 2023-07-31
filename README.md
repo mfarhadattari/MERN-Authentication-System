39a8e242446944fb91989e99472b32448f64c0c6
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-31 23:37:11 +0600

    setup mongodb connection and vercel deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # --------- | MERN Authentication System | ----------
+Client :
+Server : https://mern-auth-system.vercel.app/
--- server-side/index.js
+++ server-side/index.js
@@ -5,6 +5,27 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// Create Cli
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("MERN Authentication System Server is Running!");
 });
